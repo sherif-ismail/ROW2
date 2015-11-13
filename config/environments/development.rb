5654797f9ed57e6e5b1d1e0dd2eed2bd42018789
sherif-ismail <sherif.ismail318@gmail.com>
2015-11-14 01:01:45 +0200

    changed to send grid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,16 +14,15 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :domain => 'gmail.com',
-    :user_name      => 'rowheroku@gmail.com',
+    :user_name      => 'sherif.ismail318',
     :password       => '1234emailheroku',
     :enable_starttls_auto => true
   }
