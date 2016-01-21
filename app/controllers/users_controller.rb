bccb0fc7acf270ff752f215392f01aaf6c48099d
sherif-ismail <sherif.ismail318@gmail.com>
2016-01-21 03:11:41 +0200

    Last minutes updates for testing
    Removed Activation
    Design of home improved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,11 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
+    @user.activated = true
   	if @user.save
-      @user.send_activation_email 
-      flash[:info] = "Please check your email to activate your account."
+      #@user.send_activation_email 
+      #flash[:info] = "Please check your email to activate your account."
+      log_in @user
       redirect_to root_url
 	   else
       flash[:danger] = "Something went wrong, please try again later"
@@ -29,9 +31,9 @@ class UsersController < ApplicationController
     @user = current_user
     @user.score = params[:score]
     if @user.save
-      render 'show'
+      redirect_to root_url
     else
-      render 'show'
+      redirect_to root_url
     end
   end
 
